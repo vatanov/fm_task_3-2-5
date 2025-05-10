cea6e31c89e6ea89336abe3d93e07f759d8ad9ab
volodymyr <atanov.v@gmail.com>
2025-05-10 18:45:31 +0200

    remove extra output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,3 @@
 output "ec2_public_ip" {
   value = module.ghostfolio_web_srv.public_ip
 }
-
-output "redis_endpoint" {
-  description = "ElastiCache Redis endpoint"
-  value       = aws_elasticache_cluster.ghostfolio_redis.cache_nodes[0].address
-}
